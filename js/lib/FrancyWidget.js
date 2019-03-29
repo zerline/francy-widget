56559fc69e8ddad80c3177e7589a8be098a13c3b
Zerline <odile.benassy@u-psud.fr>
2019-03-29 16:05:01 +0100

    More code from ipywidgets. Still doing nothing.

--- js/lib/FrancyWidget.js
+++ js/lib/FrancyWidget.js
@@ -2,12 +2,41 @@ var widgets = require('@jupyter-widgets/base');
 var widgets = require('@jupyter-widgets/controls');
 var _ = require('lodash');
 
-var FrancyView = widgets.HTMLView.extend({
+var FrancyModel = widgets.StringModel.extend({
+    defaults: _.extend(widgets.StringModel.prototype.defaults(), {
+        _model_name : 'FrancyModel',
+        _view_name : 'FrancyView',
+        _model_module : 'sage-francy',
+        _view_module : 'sage-francy',
+        _model_module_version : '0.1.0',
+        _view_module_version : '0.1.0',
+        value : 'Francy Widget'
+    })
+});
+
+var FrancyView = widgets.DescriptionView.extend({
     render: function() {
-        widgets.HTMLView.prototype.render.call(this);
+        widgets.StringView.prototype.render.call(this);
+        this.el.classList.add('jupyter-widgets');
+        this.el.classList.add('widget-inline-hbox');
+        this.el.classList.add('widget-francy');
+        this.content = document.createElement('div');
+        this.content.classList.add('widget-francy-content');
+        this.el.appendChild(this.content);
+        this.update(); // Set defaults.
+    }
+
+    /**
+     * Update the contents of this view
+     */
+    update() {
+        this.content.innerHTML = this.model.get('value');
+        this.typeset(this.content);
+        return widgets.StringView.prototype.update.call(this);
     }
 });
 
 module.exports = {
+    FrancyModel : FrancyModel,
     FrancyView : FrancyView
 };
--- sage_francy/sage_francy.py
+++ sage_francy/sage_francy.py
@@ -7,12 +7,16 @@ AUTHORS ::
     Odile Bénassy, Nicolas Thiéry
 
 """
-from ipywidgets import HTML, register
+from ipywidgets import register
+from ipywidgets.widgets.widget_string import _String
 from traitlets import Unicode
 
 @register
-class FrancyWidget(HTML):
+class FrancyWidget(_String):
     """Francy widget."""
     _view_name = Unicode('FrancyView').tag(sync=True)
     _view_module = Unicode('sage-francy').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_name = Unicode('HTMLMathModel').tag(sync=True)
+    _model_module = Unicode('sage-francy').tag(sync=True)
+    _model_module_version = Unicode('^0.1.0').tag(sync=True)
