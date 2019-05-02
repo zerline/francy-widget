c34737618bef22b8cb3a9fe7125e77cdf857077b
Zerline <odile.benassy@u-psud.fr>
2019-05-02 16:22:06 +0200

    Version without javascript.

--- sage_francy/francy_widget.py
+++ sage_francy/francy_widget.py
@@ -23,12 +23,7 @@ css_lines.append(".widget-francy > .widget-francy-content {/* Fill out the area 
 @register
 class FrancyWidget(_String):
     """Francy widget."""
-    _view_name = Unicode('FrancyView').tag(sync=True)
-    _view_module = Unicode('sage-francy').tag(sync=True)
-    _view_module_version = Unicode('^0.1.0').tag(sync=True)
-    _model_name = Unicode('FrancyModel').tag(sync=True)
-    _model_module = Unicode('sage-francy').tag(sync=True)
-    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+    _view_name = Unicode('TextView').tag(sync=True)
     value = Any() # should be a networkx graph
     adapter = FrancyAdapter()
 
