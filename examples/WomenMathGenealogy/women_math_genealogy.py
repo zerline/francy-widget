800af5e6dd7c38ca3c434a698ec0af79b09e55a0
Zerline <odile.benassy@u-psud.fr>
2019-05-07 20:31:45 +0200

    Fetching data from the math genealogy project.

---
+++ examples/WomenMathGenealogy/women_math_genealogy.py
@@ -0,0 +1,37 @@
+import requests, re, json, io
+
+url_patt="https://www.genealogy.math.ndsu.nodak.edu/id.php?id=%d&fChrono=1"
+
+NOETHER = 6967
+
+patt_student = re.compile(
+    "<[\s]*tr[\s]*[^>]*>" + "[\s]*" + "<[\s]*td[^>]*>" + "[\s]*" + \
+    '<[\s]*a[\s]*href[\s]*=[\s]*"id.php\?id=([0-9]+)"[^>]*>' + "[\s]*" + \
+    "([^<]+)" + "[\s]*" + "<[\s]*/a[^>]*>" + "<[\s]*/td[^>]*>" + "[\s]*" + \
+    "<[\s]*td[\s]*[^>]*>" + "[\s]*" + "[^>]+" + "[\s]*" + "<[\s]*/td[^>]*>" + "[\s]*" + \
+    "<[\s]*td[\s]*[^>]*>" + "[\s]*" + "([0-9]*)" + "[\s]*" + \
+    "<[\s]*/td[^>]*>" + "[\s]*" + "<[\s]*td[\s]*[^>]*>" + "[\s]*" + "([0-9]*)"
+)
+
+data = { 6967 : ('6967', 'Noether, Emmy', '1907', '1405') }
+edges = []
+
+enc = json.JSONEncoder(ensure_ascii=False)
+out = open("output.txt", 'a')
+
+def loop(ident):
+    res = requests.get(url_patt % ident)
+    html = res.content.decode()
+    mm = re.findall(patt_student, html)
+    for m in mm:
+        if m[0] not in data:
+            data[m[0]] = m
+        out.write(m[1] + "\n")
+        edges.append((str(ident), m[0]))
+        print(len(data), len(edges))
+        io.open('data.json', 'w', encoding='utf8').write(enc.encode(data))
+        open("edges.json", 'w').write(enc.encode(edges))
+    for m in mm:
+        loop(int(m[0]))
+
+loop(NOETHER)
